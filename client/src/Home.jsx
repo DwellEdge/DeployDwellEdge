ef30a4d4f24feda024544ac9be710628649bdf8a
Your Name <your.email@example.com>
2026-04-21 17:18:42 +0530

    Initial MERN project setup

---
+++ client/src/Home.jsx
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <div className="homepage">
+      <header className="navbar">
+        <h2>DWELLEDGE Tech</h2>
+        <nav>
+          <ul>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Careers</li>
+            <li>Contact Us</li>
+            <li>Employee Login </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="hero">
+        <h1>Empowering Talent Transformations</h1>
+        <p>Embrace the talent revolution to remain relevant in the future.</p>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
