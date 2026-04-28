9cd4616511a858f9d439ba13ee5a82757c61d03a
Your Name <your.email@example.com>
2026-04-28 10:59:02 +0530

    Commit the changes and push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import Home from "./Home";
 import Services from "./Services";
 import ServiceData from "./ServiceData";
 import Careers from "./Careers";
+import "./style.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import ApplyJob from './ApplyJob';
+import ContactUsPage from './ContactUs';
 
 function App() {
   return (
@@ -13,7 +18,8 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/services/:serviceId" element={<ServiceData />} />
           <Route path="/Careers" element={<Careers />} />
-          
+          <Route path="/apply" element={<ApplyJob />} />
+          <Route path="/Contact" element={<ContactUsPage />} />
         </Routes>
       </BrowserRouter>
     </div>
